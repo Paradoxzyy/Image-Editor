038e576d424c15c2ad3a3932645868c7513d9712
Paradoxzyy <228410201+Paradoxzyy@users.noreply.github.com>
2026-09-04 16:30:39 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
 window.onload = () => {
   const uploader = document.getElementById("uploader")
+  const uploaderButton = document.getElementById("uploader-button")
   const canvas = document.getElementById("canvas")
   const ctx = canvas.getContext("2d")
   
-  uploader.onchange = e => {
-    const file = e.target.files[0]
+  //----------------------------------------
+  //uploader.onchange = e => {
+    //const file = e.target.files[0]
+  uploaderButton.onclick = () => {
+    const file = uploader.files[0]
     const reader = new FileReader()
     
-    reader.onload = function(event) {
+    reader.onload = e => {
       const img = new Image()
   
-      img.onload = function() {
+      img.onload = () => {
         canvas.width = img.width
         canvas.height = img.height
         ctx.drawImage(img, 0, 0)
       }
   
-      img.src = event.target.result
+      img.src = e.target.result
     }
   
     reader.readAsDataURL(file)
   }
-}
 
-//crop(0, 0, 200, 200)
-function crop(x, y, width, height) {
-  const temp = document.createElement("canvas")
-  const tctx = temp.getContext("2d")
-  temp.width = width
-  temp.height = height
-  tctx.drawImage(canvas, x, y, width, height, 0, 0, width, height)
-  canvas.width = width
-  canvas.height = height
-  ctx.drawImage(temp, 0, 0)
-}
+  //----------------------------------------
+  const crop = (x, y, width, height) => {
+    const temp = document.createElement("canvas")
+    const tctx = temp.getContext("2d")
+    temp.width = width
+    temp.height = height
+    tctx.drawImage(canvas, x, y, width, height, 0, 0, width, height)
+    canvas.width = width
+    canvas.height = height
+    ctx.drawImage(temp, 0, 0)
+  }
 
-function download(format = "image/png") {
-  const link = document.createElement("a")
-  link.download = "output." + format.split("/")[1]
-  link.href = canvas.toDataURL(format)
-  link.click()
+  //----------------------------------------
+  const download = (format = "image/png") => {
+    const link = document.createElement("a")
+    link.download = "output." + format.split("/")[1]
+    link.href = canvas.toDataURL(format)
+    link.click()
+  }
 }
